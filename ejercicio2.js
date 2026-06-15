c8210c6d9c04088d215ae6f8ae1d567b27446a13
Daniela <garciapozoerikadaniela@gmail.com>
2026-06-15 16:49:11 -0600

    ejercicios de clases

---
+++ ejercicio2.js
@@ -0,0 +1,27 @@
+const hechos= [ {padre: 'Juan', hijo: 'Luis' },
+{ padre: 'Juan', hijo: 'Pedro' },
+{ padre: 'Abraham', hijo: 'Juan' }, ];
+
+function SonHermanos(persona1, persona2) {
+    return hechos.some(humano1 =>
+        hechos.some(humano2 =>
+            humano1.hijo === persona1 &&
+            humano2.hijo === persona2 &&
+            humano1.padre === humano2.padre &&
+            persona1 !== persona2
+        )
+    );
+}
+
+function EsAbuelo(abuelo, nieto) {
+    return hechos.some(humano1 =>
+        hechos.some(humano2 =>
+            humano1.padre === abuelo &&
+            humano2.hijo === nieto &&
+            humano1.hijo === humano2.padre
+        )
+    );
+}
+
+console.log(SonHermanos('Luis', 'Pedro')); 
+console.log(SonHermanos('Luis', 'Juan'));
